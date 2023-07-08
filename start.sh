6b85aff74c661403f952a427856359ab9b0456db
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2023-07-08 12:48:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ritheshrkrm/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/Nxt-Stark/Elizabeth-Olsen-V2.git /Elizabeth-Olsen-V2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /Elizabeth-Olsen-V2
 fi
-cd /PiroAutoFilterBot
+cd /Elizabeth-Olsen-V2
 pip3 install -U -r requirements.txt
 echo "Starting PiroAutoFilterBot...."
 python3 bot.py
