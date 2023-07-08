e2083c473431110489be629962761b6aafd95c67
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2023-07-08 12:57:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -46,7 +46,7 @@ async def start(client, message):
            ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
-            photo=random.choice(PICS),
+            photo='https://telegra.ph/file/e9d0e40ed770b4d25cce8.jpg',
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
